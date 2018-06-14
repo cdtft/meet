378b7a25d52d238c38d06d9f0370e40833168743
wangcheng <453451180@qq.com>
2018-06-14 23:44:32 +0800

    :ambulance: test
    王城 #1

--- src/main/java/com/cdut/annotation/Authorization.java
+++ src/main/java/com/cdut/annotation/Authorization.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * 自定义注解该注解在controller中的方法上将会检查用户是否登陆
  * 如果没有登陆将会返回401未授权错误
- * Created by king on 2017/9/11.
+ * Created by king on 2017/9/11
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
